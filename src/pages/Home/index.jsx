df7efd09281e0590978d91214f15b06d855c0a6a
Yulany Munevar <andra_852@hotmail.com>
2022-11-30 20:24:57 -0500

    fixing home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -17,7 +17,7 @@ const Home = () => {
       <header className="home__head">
         <div className="home__iconStore">
           <img src={eunoia} alt="" className="home__image" />
-          <p className="home__storeName">Store</p>
+          <p className="home__storeName">Welcome to our Store</p>
         </div>
         <div className="home__navigation">
           <button type="submit" className="home__about" onClick={handleSubmit}>About</button>
